c512aa00f9b093d5a404b0a9828aa61ffd1ef739
jaime oliver <jaime.oliver@clarity.ai>
2022-02-03 06:18:55 -0800

    Pushed from Domino: https://domino.clarity.ai/u/jaime_oliver/social_capital/runs/61fbb8d1d7512012ebb1b078

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='social_capital',
     packages=find_packages(),
     version='0.1.0',
     description='This repo contains the code for the study',
